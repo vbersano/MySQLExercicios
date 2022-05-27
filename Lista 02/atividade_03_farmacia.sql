67d9cafc77aadbfa1c4997a25807b50aacab9428
Vinicius Bersano <tk3.bersano@hotmail.com>
2022-05-27 13:47:48 -0300

    alteracoes

--- Lista 02/atividade_03_farmacia.sql
+++ Lista 02/atividade_03_farmacia.sql
@@ -2,37 +2,44 @@ CREATE DATABASE db_farmacia_bem_estar;
 
 USE db_farmacia_bem_estar;
 
-CREATE TABLE tb_categorias (
-id BIGINT NOT NULL AUTO_INCREMENT,
-tarja VARCHAR (255) NOT NULL,
-generico BOOLEAN DEFAULT false,
+CREATE TABLE tb_categorias
+(
+id BIGINT AUTO_INCREMENT,
+tarja VARCHAR (255),
+generico VARCHAR (255),
 
-PRIMARY KEY (id)
+PRIMARY KEY(id)
 );
 
 INSERT INTO tb_categorias (tarja, generico) VALUES
-("VENDA SOBRE PRESCRIÇÃO MÉDICA", true),
-("VENDA SOBRE PRESCRIÇÃO MÉDICA", false),
-("ISENTO DE PRESCRIÇÃO", false),
-("ISENTO DE PRESCRIÇÃO", true)
+("VENDA SOBRE PRESCRIÇÃO MÉDICA", "GENERICO"),
+("VENDA SOBRE PRESCRIÇÃO MÉDICA", "NÃO GENERICO"),
+("ISENTO DE PRESCRIÇÃO", "NÃO GENERICO"),
+("ISENTO DE PRESCRIÇÃO", "GENERICO")
 ;
 
-CREATE TABLE tb_produtos (
-id BIGINT NOT NULL AUTO_INCREMENT,
-nome VARCHAR (255) NOT NULL,
-validade DATE NOT NULL,
+CREATE TABLE tb_produtos
+(
+id BIGINT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR (255),
+validade DATE,
 laboratorio VARCHAR (255),
-preco DECIMAL (8,2) NOT NULL,
+preco DECIMAL (8,2),
+tarja_id BIGINT,
 
-PRIMARY KEY (id),
-FOREIGN KEY (id) REFERENCES tb_categorias(id)
+FOREIGN KEY (tarja_id) REFERENCES tb_categorias(id)
 );
 
-INSERT INTO tb_produtos (nome, validade, laboratorio, preco) VALUES 
-("Ivermectina", '2026-10-22', "EMS", 55.00),
-("Venvanse", '2024-09-05', "Shire", 300.00),
-("Benegrip", '2023-01-18', "Hypera", 20.00),
-("Loratadina", '2023-11-07', "Neo Quimica", 10.00);
+
+INSERT INTO tb_produtos (nome, validade, laboratorio, preco) VALUES ("Ivermectina", '2026-10-22', "EMS", 55.00);
+INSERT INTO tb_produtos (nome, validade, laboratorio, preco) VALUES ("Venvanse", '2024-09-05', "Shire", 300.00);
+INSERT INTO tb_produtos (nome, validade, laboratorio, preco) VALUES ("Benegrip", '2023-01-18', "Hypera", 20.00);
+INSERT INTO tb_produtos (nome, validade, laboratorio, preco) VALUES ("Loratadina", '2023-11-07', "Neo Quimica", 10.00);
+INSERT INTO tb_produtos (nome, validade, laboratorio, preco) VALUES ("Viagra", '2022-11-11', "Pfizer", 70.00);
+INSERT INTO tb_produtos (nome, validade, laboratorio, preco) VALUES ("Lipitor", '2025-12-06', "Pfizer", 50.00);
+INSERT INTO tb_produtos (nome, validade, laboratorio, preco) VALUES ("Tylenol", '2023-02-26', "Johnson & Johnson", 30.00);
+INSERT INTO tb_produtos (nome, validade, laboratorio, preco) VALUES ("Doril", '2022-05-10', "Cosmed", 24.99);
+
 
 SELECT * FROM tb_produtos INNER JOIN tb_categorias
 ON tb_produtos.id = tb_categorias.id;
@@ -48,4 +55,3 @@ WHERE preco > 5.00 AND preco < 60.00;
 SELECT * FROM tb_produtos INNER JOIN tb_categorias
 ON tb_produtos.id = tb_categorias.id
 WHERE nome LIKE "%c%";
-
--- Lista 02/atividade_04_curso.sql
+++ Lista 02/atividade_04_curso.sql
@@ -1,16 +1,15 @@
 CREATE DATABASE db_curso_da_minha_vida DEFAULT CHARACTER SET utf8 ;
 
+
 USE db_curso_da_minha_vida;
 
-CREATE TABLE tb_cursos (
+CREATE TABLE tb_categorias (
   id BIGINT NOT NULL AUTO_INCREMENT,
-  valor DECIMAL(8,2) NOT NULL,
-  periodo VARCHAR(255) NOT NULL,
-  filial VARCHAR(255) NULL,
-  duracaoMeses SMALLINT NULL,
-  
-  PRIMARY KEY (id)
-  );
+  materia VARCHAR(255) NULL,
+  professor VARCHAR(255) NULL,
+	
+    PRIMARY KEY (id)
+);
 
 INSERT INTO tb_categorias (materia, professor) VALUES 
 ("Lógica de Programação em Java", "Andersson"),
@@ -20,16 +19,17 @@ INSERT INTO tb_categorias (materia, professor) VALUES
 ("Infraestrutura de Redes", "Augusto");
 
 
-CREATE TABLE IF NOT EXISTS tb_categorias (
+CREATE TABLE tb_cursos (
   id BIGINT NOT NULL AUTO_INCREMENT,
-  materia VARCHAR(255) NULL,
-  professor VARCHAR(255) NULL,
-  tb_cursos_id BIGINT NOT NULL,
-	
-    PRIMARY KEY (id),
-
-    FOREIGN KEY (tb_cursos_id) REFERENCES tb_cursos (id)
-);
+  valor DECIMAL(8,2),
+  periodo VARCHAR(255),
+  filial VARCHAR(255),
+  duracaoMeses SMALLINT,
+  tb_cursos_id BIGINT,
+  
+  PRIMARY KEY (id),
+  FOREIGN KEY (tb_cursos_id) REFERENCES tb_cursos (id)
+  );
 
 INSERT INTO tb_cursos (valor, periodo, filial, duracaoMeses) VALUES 
 (449.99, "NOTURNO", "SÃO PAULO", 3),
